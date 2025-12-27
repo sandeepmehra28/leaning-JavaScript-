27226a5799cd06a058814a4182dd025d66fa9e47
sandeep mehra <sandeepmehra2259@gmail.com>
2025-12-27 23:22:35 +0530

    update and add new files in js

---
+++ js_Folders/Practice_Set/q1.js
@@ -0,0 +1,22 @@
+// Q1: Write a program to concatenate a string and a number.
+// let name = "sandeep";
+// let num = 5;
+// console.log("This is a concatenation : "+ (name+num));
+
+
+//Q2.
+// let a1 = {
+//   name: "Harry",
+//   section: 1,
+//   isPrincipal: false
+// }
+// a1 = 45;
+// console.log(a1);
+
+//Q3
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work."
+}
+console.log(dict['yakka'])
